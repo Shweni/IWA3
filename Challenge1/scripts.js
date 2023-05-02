7eeecdae24717d0bd42df304cd428036ae4ca0ce
Lindokuhle Shweni <127991096+Shweni@users.noreply.github.com>
2023-05-02 19:43:14 +0200

    Add files via upload

---
+++ Challenge1/scripts.js
@@ -0,0 +1,7 @@
+//scripts.js
+
+'import company form ';'configuration.js'
+'import year form =';'configuration.js'
+
+const message = '© ' + Company  + ' (' + year + ')'
+document.querySelector('footer').innerText =  message
